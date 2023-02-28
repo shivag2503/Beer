aedad1bfc36000cc4d66f74b9640aea961387544
shivag2503 <shivamagrawal1166171@gmail.com>
2023-02-28 21:52:30 +0530

    mokito

--- src/test/java/com/barclays/practice/springmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/barclays/practice/springmvc/controllers/BeerControllerTest.java
@@ -1,21 +1,43 @@
 package com.barclays.practice.springmvc.controllers;
 
+import com.barclays.practice.springmvc.domain.Beer;
+import com.barclays.practice.springmvc.services.BeerService;
+import com.barclays.practice.springmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl beerImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+
+        Beer testBeer = beerImpl.getBeers().get(0);
+
+        given(beerService.getBearById(any(UUID.class))).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
